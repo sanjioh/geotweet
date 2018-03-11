313361ce34d9f76f3fba6d2ff7e3598565a4ce71
Fabio Sangiovanni <sjh+github@sanjioh.org>
2018-03-11 17:07:23 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+NAME = 'geotweet'
+DESCRIPTION = 'Stream geolocalized tweets and plot them on a map.'
+URL = 'https://github.com/sanjioh/geotweet'
+AUTHOR = 'Fabio Sangiovanni'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '1.0.0'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': ['geotweet=geotweet.geotweet:main'],
+    },
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
