83bbe5f497258b1d57e0af48c0f15bc3ed44a690
Fabio Sangiovanni <sjh+github@sanjioh.org>
2018-03-12 22:32:19 +0100

    docstrings

--- src/geotweet/formatters.py
+++ src/geotweet/formatters.py
@@ -1,4 +1,18 @@
+"""
+Formatters module.
+
+The class observer.ConsoleObserver uses a formatter to render a
+string representation of a tweet to the terminal, according to
+some particular formatting strategy.
+This module can be extended with other formatter classes; they only
+need to implement a format() method that takes as argument an
+observers.Tweet instance.
+"""
+
+
 class SimpleFormatter:
+    """Create simple string representations of tweets."""
+
     _format_string = (
         'User: @{tweet.user_screen_name} ({tweet.user_name})\n'
         'Location: {tweet.longitude} longitude, {tweet.latitude} latitude\n'
@@ -6,4 +20,5 @@ class SimpleFormatter:
     )
 
     def format(self, tweet):
+        """Build a string representation of a tweet."""
         return self._format_string.format(tweet=tweet)
--- src/geotweet/listener.py
+++ src/geotweet/listener.py
@@ -1,13 +1,33 @@
+"""
+Listener module.
+
+This module contains the GeoStreamListener class.
+"""
+
 import tweepy
 
 
 class GeoStreamListener(tweepy.StreamListener):
+    """
+    Provide hooks to invoke when certain stream-related events happen.
+
+    As a subclass of tweepy.StreamListener, this class
+    is the main bridge with the tweepy streaming library.
+    It's methods are hooks that the tweepy machinery invokes
+    when something relevant happens (new tweets, unhandled errors,
+    timeouts, and so on).
+    This implementation shuts the program down whenever something
+    erroneous or unexpected occurs.
+    """
+
     def __init__(self, runner, observers=None):
+        """Initialize a GeoStreamListener object."""
         self._runner = runner
         self._observers = observers or []
         super().__init__()
 
     def on_status(self, status):
+        """Notify all the observers of a new geolocalized tweet."""
         if status.coordinates is not None:
             for observer in self._observers:
                 observer.update(status)
@@ -17,16 +37,21 @@ class GeoStreamListener(tweepy.StreamListener):
         self._runner.stop()
 
     def on_exception(self, exception):
+        """Handle exceptions."""
         self._exit(f'unhandled exception ({exception!a})')
 
     def on_error(self, status_code):
+        """Handle unexpected HTTP status codes coming from Twitter APIs."""
         self._exit(f'status code {status_code}')
 
     def on_timeout(self):
+        """Handle stream timeouts."""
         self._exit('timeout')
 
     def on_disconnect(self, notice):
+        """Handle stream disconnections."""
         self._exit('disconnected ({notice})')
 
     def on_warning(self, notice):
+        """Handle Twitter warning notices."""
         self._exit('warning received ({notice})')
